51c8a4a7fb9262cd3f91114d76f455ca9245011f
basil1987 <basil1987@gmail.com>
2022-06-24 20:21:41 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,7 +47,7 @@ resource "aws_instance" "web001" {
   ami           = "ami-0851b76e8b1bce90b"
   instance_type = "t2.micro"
   vpc_security_group_ids = [ aws_security_group.webservers.id ]
-  key_name = "ansible-horizon"
+  key_name = "ansible-server-b"
   tags = {
     Name = "Web-${count.index + 1}"
     type = "webserver"
