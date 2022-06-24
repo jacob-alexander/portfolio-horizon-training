9876f13c347f074599f674e463d501924f58be36
basil1987 <basil1987@gmail.com>
2022-06-24 21:05:52 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,7 +43,7 @@ resource "aws_security_group" "webservers" {
 }
 
 resource "aws_instance" "web001" {
-  count = "2"
+  count = "5"
   ami           = "ami-0851b76e8b1bce90b"
   instance_type = "t2.micro"
   vpc_security_group_ids = [ aws_security_group.webservers.id ]
