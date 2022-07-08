d8c8281091567806bae7ee0d6d2d681dc7b61dd2
jacob-alexander <vivekjacobalex@gmail.com>
2022-07-08 16:42:29 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,16 +43,14 @@ resource "aws_security_group" "webservers" {
 }
 
 resource "aws_instance" "web001" {
-  count = "5"
-  ami           = "ami-0851b76e8b1bce90b"
+  count = "1"
+  ami           = "ami-006d3995d3a6b963b"
   instance_type = "t2.micro"
   vpc_security_group_ids = [ aws_security_group.webservers.id ]
-  key_name = "ansible-server-b"
+  key_name = "fedora_vivekjacob"
   tags = {
     Name = "Web-${count.index + 1}"
     type = "webserver"
     App = "portfolio_webserver"
   }
 }
-
-
